9870a224138ce3aa40b47a9bc2e4ac31062410ac
aszos <aszostek@udel.edu>
2014-09-06 17:14:46 -0400

    removed list stuff from frontend

--- server/publish.js
+++ server/publish.js
@@ -1,22 +1,8 @@
-// Lists -- {name: String}
-Lists = new Mongo.Collection("lists");
 
-// Publish complete set of lists to all clients.
-Meteor.publish('lists', function () {
-  return Lists.find();
-});
-
-
-// Todos -- {text: String,
-//           done: Boolean,
-//           tags: [String, ...],
-//           list_id: String,
-//           timestamp: Number}
-Todos = new Mongo.Collection("todos");
+Cams = new Mongo.Collection("cams");
 
 // Publish all items for requested list_id.
-Meteor.publish('todos', function (list_id) {
-  check(list_id, String);
-  return Todos.find({list_id: list_id});
+Meteor.publish('cams', function () {
+  return Cams.find();
 });
 
