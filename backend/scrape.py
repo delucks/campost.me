c31468c1cd6030c70879eba181662338215c29e3
delucks <jamie.delucks@gmail.com>
2014-09-06 13:47:12 -0400

    Added panasonic mjpg support

--- backend/scrape.py
+++ backend/scrape.py
@@ -32,16 +32,16 @@ def getnpages(query,n):
         links = links + scrapegs(url)
     return links
 
+# checks for the category, if not, it attempts a HTTP request and checks the error code
+# return: True for site up, False for site down
 def checksite(site,category):
-    # checks for the category, if not, it attempts a HTTP request and checks the error code
-    # return: True for site up, False for site down
     if (category == "mobotix"):
         url = site.replace("/control/userimage.html","/record/current.jpg")
         try:
             r = requests.get(url,headers=headers)
             return (r.status_code == 200)
         except requests.exceptions.ConnectionError:
-            # fuck yo errors
+            # fuck yo connection
             return False
     elif (category == "axismjpg"):
         # there's no way to open the mjpg without reading data
@@ -64,6 +64,7 @@ def checksite(site,category):
         except requests.exceptions.ConnectionError:
             return False
 
+# not sure what this should return yet. takes an image from the current webcam feed
 def capturesite(site,category):
     if (category == "mobotix"):
         url = site.replace("/control/userimage.html","/record/current.jpg")
@@ -78,25 +79,41 @@ def capturesite(site,category):
                 varpath = line.split("\"")[1] # dirty hack to get the relative path to the mjpg
         url = "http://"+site.split("/")[2]+varpath
         mj = MjpegParser(url)
-        im = mj.serve()
-        with open(str(time.time())+".jpg",'wb') as filename:
-            filename.write(im.as_image().read())
+        try:
+            im = mj.serve()
+            with open(str(time.time())+".jpg",'wb') as filename:
+                filename.write(im.as_image().read())
+        except AttributeError:
+            pass # mjpegparser can fuck up its __init__() on occasion. it still works.
     elif (category == "webcamxp"):
         url = site + "/cam_1.jpg"
         outfolder = "."
         filename = int(time.time())
         outpath = os.path.join(outfolder,filename)
         urllib.urlretrieve(url, outpath)
+    elif (category == "panamjpg"):
+        url = "http://"+site.split("/")[2]+"/nphMotionJpeg?Resolution=640x480&Quality=Clarity"
+        mj = MjpegParser(url)
+        try:
+            im = mj.serve()
+            # check if the resolution is less than 320 by 240- if it is, the cam is off
+            if im.im.size[0] >= 320 and im.im.size[1] >= 240:
+                with open(str(time.time())+".jpg",'wb') as filename:
+                    filename.write(im.as_image().read())
+        except AttributeError:
+            pass
 
-# categories- mobotix, axismjpg, webcamxp
+# categories- mobotix, axismjpg, webcamxp, panamjpg
 search_mobotix = "inurl:/control/userimage.html"
 search_axismjpg = "inurl:/view/index.shtml axis"
 search_webcamxp = 'intitle:"my webcamXP server!"'
+search_panamjpg = 'inurl:"CgiStart?page=Single"'
 
 test_mobotix = "http://www.videovalvonta.fi"
 test_axismjpg = "http://axis-78tramore.axiscam.net/view/viewer_index.shtml"
+test_panamjpg = "http://62.2.213.149/CgiStart?page=Single&Resolution=640x480&Quality=Clarity&RPeriod=0&Size=STD&PresetOperation=Move&Language=0"
 
-stuff = getnpages(search_axismjpg,3)
+stuff = getnpages(search_panamjpg,2)
 for item in stuff:
-    print checksite(item,"axismjpg")
-#capturesite(test_axismjpg,"axismjpg")
+    #print checksite(item,"axismjpg")
+    capturesite(item,"panamjpg")
