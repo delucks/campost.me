a72d9a084df8799f0aacd70b44f83adf22131efc
delucks <jamie.delucks@gmail.com>
2014-09-06 13:05:13 -0400

    Added axismjpg and webcamXP

--- backend/scrape.py
+++ backend/scrape.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from mjpegtools import MjpegParser
 import os
 import urllib
 import time
@@ -33,16 +34,28 @@ def getnpages(query,n):
 
 def checksite(site,category):
     # checks for the category, if not, it attempts a HTTP request and checks the error code
+    # return: True for site up, False for site down
     if (category == "mobotix"):
         url = site.replace("/control/userimage.html","/record/current.jpg")
-        #print url
-        #r = requests.get(url,headers=headers)
-        #return (r.status_code == 200)
         try:
             r = requests.get(url,headers=headers)
             return (r.status_code == 200)
         except requests.exceptions.ConnectionError:
             # fuck yo errors
+            return False
+    elif (category == "axismjpg"):
+        # there's no way to open the mjpg without reading data
+        try:
+            r = requests.get(site,headers=headers)
+            return (r.status_code == 200)
+        except requests.exceptions.ConnectionError:
+            return False
+    elif (category == "webcamxp"):
+        url = site + "/cam_1.jpg"
+        try:
+            r = requests.get(url,headers=headers)
+            return (r.status_code == 200)
+        except requests.exceptions.ConnectionError:
             return False
     else:
         try:
@@ -59,19 +72,31 @@ def capturesite(site,category):
         outpath = os.path.join(outfolder,filename)
         urllib.urlretrieve(url, outpath)
     elif (category == "axismjpg"):
-        r = requests.get(site, headers=headers)
-        rootsoup = BeautifulSoup(r.text)
-        print rootsoup.prettify()
-        # url = rootsoup.find("frame")
-        # print url
+        r = urllib.urlopen(site) #requests.get(site, headers=headers)
+        for line in r.readlines():
+            if "var imagepath" in line:
+                varpath = line.split("\"")[1] # dirty hack to get the relative path to the mjpg
+        url = "http://"+site.split("/")[2]+varpath
+        mj = MjpegParser(url)
+        im = mj.serve()
+        with open(str(time.time())+".jpg",'wb') as filename:
+            filename.write(im.as_image().read())
+    elif (category == "webcamxp"):
+        url = site + "/cam_1.jpg"
+        outfolder = "."
+        filename = int(time.time())
+        outpath = os.path.join(outfolder,filename)
+        urllib.urlretrieve(url, outpath)
 
+# categories- mobotix, axismjpg, webcamxp
 search_mobotix = "inurl:/control/userimage.html"
 search_axismjpg = "inurl:/view/index.shtml axis"
+search_webcamxp = 'intitle:"my webcamXP server!"'
 
 test_mobotix = "http://www.videovalvonta.fi"
 test_axismjpg = "http://axis-78tramore.axiscam.net/view/viewer_index.shtml"
 
-#stuff = getnpages(search_mobotix,3)
-#for item in stuff:
-#    print checksite(item,"mobotix")
-capturesite(test_axismjpg,"axismjpg")
+stuff = getnpages(search_axismjpg,3)
+for item in stuff:
+    print checksite(item,"axismjpg")
+#capturesite(test_axismjpg,"axismjpg")
