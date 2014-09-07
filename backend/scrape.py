19277ce463985fe15831143b4486242bbc849eef
delucks <jamie.delucks@gmail.com>
2014-09-07 05:11:48 -0400

    Fixed datatypes and added image comprehension for a few of them

--- backend/scrape.py
+++ backend/scrape.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from mjpegtools import MjpegParser
+from PIL import Image
 import os
+import errno
 import Queue
 import urllib
 import time
@@ -19,6 +21,10 @@ headers = {'User-Agent': USER_AGENT}
 # the number of pages of google search results you want returned each time. 10 per page
 PAGE_AMOUNT = 15
 DEBUG = True
+if DEBUG:
+    numthreads = 1
+else:
+    numthreads = 10
 
 # categories- mobotix, axismjpg, webcamxp, panamjpg (two searches)
 search_mobotix = "inurl:/control/userimage.html"
@@ -42,46 +48,99 @@ def dp(message):
 
 # scrape a page of google search results for the links it contains
 def scrapegs(url):
-    r = requests.get(url, headers=headers)
-    rootsoup = BeautifulSoup(r.text)
+    #r = requests.get(url, headers=headers)
+    fh = open(url,'r')
+    rootsoup = BeautifulSoup(fh.read())
     links = []
     for post in rootsoup.find_all('li', "g"):
         links.append(post.div.h3.a.get('href'))
     return links
 
 # get n pages of a search result, dump their urls into an array
+# query used to be the actual google query but fuck google so it's now the dork! :D
 def getnpages(query,n):
-    links = []
-    basepage = "https://www.google.com/search?safe=off&q="
-    san = urllib.quote(query)
+    # add in pages manually here
+    if query == "axismjpg":
+        links = [ "http://webcam.uoregon.edu/en/index.html","http://wc2.dartmouth.edu/view/index.shtml" ]
+    else:
+        links = [ "http://webcam.uoregon.edu/en/index.html" ]
+    #basepage = "https://www.google.com/search?safe=off&q="
+    #san = urllib.quote(query)
+    # also used to be range 0,n but fuck google
     for i in range(0,n):
-        url = basepage + san + "&start=" + str(i*10)
+        #url = basepage + san + "&start=" + str(i*10)
+        url = "backend/.responses/response-"+query+"-"+str(n)+".html"
         links = links + scrapegs(url)
     return links
 
 # checks for the dorktype, if not, it attempts a HTTP request and checks the error code
 # return: True for site up, False for site down
 def checksite(site,dorktype):
-    if (dorktype == "mobotix"):
+    if (site == "http://webcam.uoregon.edu/en/index.html"):
+        dp("checksite uoregon")
+        url = "http://webcam.uoregon.edu/oneshotimage1"
+        try:
+            dp("checksite request")
+            r = requests.get(url,headers=headers)
+            dp(r.status_code)
+            return (r.status_code == 200)
+        except requests.exceptions.ConnectionError:
+            dp("checksite connection error")
+            return False
+    if (dorktype == "direct"):
+        url = site
+        dp("checksite direct")
+        try:
+            dp("checksite request")
+            r = requests.get(url,headers=headers)
+            dp(r.status_code)
+            return (r.status_code == 200)
+        except requests.exceptions.ConnectionError:
+            dp("checksite connection error")
+            return False
+    elif (dorktype == "mobotix"):
+        dp("checksite mobotix")
         url = site.replace("/control/userimage.html","/record/current.jpg")
         try:
+            dp("checksite request")
             r = requests.get(url,headers=headers)
+            dp(r.status_code)
             return (r.status_code == 200)
         except requests.exceptions.ConnectionError:
             # fuck yo connection
+            dp("checksite connection error")
             return False
     elif (dorktype == "webcamxp"):
+        dp("checksite webcamxp")
         url = site + "/cam_1.jpg"
         try:
+            dp("checksite request")
             r = requests.get(url,headers=headers)
+            dp(r.status_code)
             return (r.status_code == 200)
         except requests.exceptions.ConnectionError:
+            dp("checksite connection error")
+            return False
+    elif (dorktype == "axismjpg"):
+        dp("checksite axismjpg")
+        url = site.replace("/index.shtml","/snapshot.shtml?picturepath=/jpg/image.jpg&width=640")
+        try:
+            dp("checksite request")
+            r = requests.get(url,headers=headers)
+            dp(r.status_code)
+            return (r.status_code == 200)
+        except requests.exceptions.ConnectionError:
+            dp("checksite connection error")
             return False
     else:
+        dp("checksite generic")
         try:
+            dp("checksite request")
             r = requests.get(site,headers=headers)
+            dp(r.status_code)
             return (r.status_code == 200)
         except requests.exceptions.ConnectionError:
+            dp("checksite connection error")
             return False
 
 # utility method, saves a frame of the website to a path generated using 'id'. should return a boolean describing the success of the operation. we'll see how that turns out
@@ -90,31 +149,53 @@ def capturesite(site,dorktype,postid):
     outfolder = "public"
     subfolder = str(postid)
     filename = str(int(time.time())) + ".jpg"
+    # first make the dir for the post if it doesn't exist
+    idpath = os.path.join(outfolder,subfolder)
+    try:
+        os.makedirs(idpath)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
     outpath = os.path.join(outfolder,subfolder,filename)
+    if (site == "http://webcam.uoregon.edu/en/index.html"):
+        url = "http://webcam.uoregon.edu/oneshotimage1"
+        urllib.urlretrieve(url, outpath)
+        return True
     if (dorktype == "mobotix"):
         url = site.replace("/control/userimage.html","/record/current.jpg")
         urllib.urlretrieve(url, outpath)
         return True
     elif (dorktype == "axismjpg"):
-        r = urllib.urlopen(site) #requests.get(site, headers=headers)
-        for line in r.readlines():
-            if "var imagepath" in line:
-                varpath = line.split("\"")[1] # dirty hack to get the relative path to the mjpg
-        url = "http://"+site.split("/")[2]+varpath
-        mj = MjpegParser(url)
+        url = site.replace("/index.shtml","/snapshot.shtml?picturepath=/jpg/image.jpg&width=640")
+        urllib.urlretrieve(url, outpath)
         try:
-            im = mj.serve()
-            with open(outpath,'wb') as filename:
-                filename.write(im.as_image().read())
+            im = Image.open(outpath)
             return True
-        except AttributeError:
-            pass # mjpegparser can fuck up its __init__() on occasion. it still works.
-            return False
+        except IOError:
+            # not a valid image file
+            os.unlink(outpath)
+            return False 
+        #r = urllib.urlopen(site) #requests.get(site, headers=headers)
+        #varpath = ""
+        #for line in r.readlines():
+        #    if "var imagepath" in line:
+        #        varpath = line.split("\"")[1] # dirty hack to get the relative path to the mjpg
+        #if varpath is not "":
+        #    url = "http://"+site.split("/")[2]+varpath
+        #    mj = MjpegParser(url)
+        #    try:
+        #        im = mj.serve()
+        #        with open(outpath,'wb') as filename:
+        #            filename.write(im.as_image().read())
+        #        return True
+        #    except AttributeError:
+        #        pass # mjpegparser can fuck up its __init__() on occasion. it still works.
+        #        return False
     elif (dorktype == "webcamxp"):
         url = site + "/cam_1.jpg"
         urllib.urlretrieve(url, outpath)
         return True
-    elif (dorktype == "panamjpg" or dorktype == "panamjpg1"):
+    elif (dorktype == "panamjpg" or dorktype == "panamjpg_1"):
         url = "http://"+site.split("/")[2]+"/nphMotionJpeg?Resolution=640x480&Quality=Clarity"
         mj = MjpegParser(url)
         try:
@@ -134,6 +215,7 @@ def capturesite(site,dorktype,postid):
 # check if link already exists in the DB. Returns True if the record exists, false otherwise
 def linkexists(url,mongoc):
     record = mongoc.find_one({"url": url})
+    dp("linkexists returns: "+str(record))
     return (record is not None)
 
 def detectsoftware(url):
@@ -145,7 +227,7 @@ def detectsoftware(url):
    elif "CgiStart?page=Single" in url:
        return "panamjpg"
    elif "ViewerFrame?Mode=Motion" in url:
-       return "panamjpg1"
+       return "panamjpg_1"
    else:
        # TODO: finish this method
        r = requests.get(url, headers=headers)
@@ -159,7 +241,11 @@ def insert(url,user,dorktype,mongoc,geoip):
    current = int(time.time()) 
    # the colon takes care of errant port numbers
    hostname = url.split("/")[2].split(":")[0]
+   #try:
    addr = socket.gethostbyaddr(hostname)[2][0]
+   #except Exception:
+   #    print hostname
+   #    raise SystemExit
    tags = []
    location = geoip.country_name_by_name(addr) # sure, it's by name, but it takes addresses too! have an open mind
    if user is None:
@@ -176,6 +262,12 @@ def insert(url,user,dorktype,mongoc,geoip):
            "softwareType": dorktype}
    return mongoc.insert(record)
 
+def uniq(array):
+    d = {}
+    for a in array:
+        d[a] = 1
+    return d.keys()
+
 # builds a queue of pages to be checked and scraped
 def scrapequeue(dorktype,queue):
     if dorktype == "mobotix":
@@ -186,14 +278,15 @@ def scrapequeue(dorktype,queue):
         search = search_webcamxp
     elif dorktype == "panamjpg":
         search = search_panamjpg
-    elif dorktype == "panamjpg1":
-        search = search_panamjpg1
+    elif dorktype == "panamjpg_1":
+        search = search_panamjpg_1
     else:
         search = search_mobotix #default
-    print search
-    for item in getnpages(search,15):
+    dp("Searching: "+dorktype)
+    for item in uniq(getnpages(dorktype,14)):
         dp("Inserting "+item+" into the queue.")
         queue.put(item)
+    dp("")
 
 # thread-safe worker that will process each page in the queue
 # params: queue to pull from, threadID for debugging, dorktype so we know what to process
@@ -209,11 +302,13 @@ def scrapeWorker(queue,threadID,dorktype,mongoc,geoip):
                 if checksite(url,dorktype):
                     dp(threadID+" site ok  " + url)
                     postid = insert(url,None,dorktype,mongoc,geoip)
-                    dp(threadID+" inserted " + postid)
+                    dp(threadID+" inserted " + str(postid))
                     success = capturesite(url,dorktype,postid)
-                    dp(threadID+" imaged?? " + success)
                     if not success:
-                        queue_available = False # die on failure for now
+                        #mongoc.remove(postid)
+                        dp(threadID+" imaging failed.")
+                    else:
+                        dp(threadID+" imaging succeeded.")
         except Queue.Empty:
             dp(threadID+" finished.")
             queue_available = False
@@ -228,14 +323,15 @@ mc = pymongo.MongoClient('127.0.0.1',3001)
 mdb = mc.meteor
 
 # set up the queue for scraping (temporary, for testing purposes)
-#q = Queue.Queue()
-#scrapequeue("mobotix",q)
-#for i in range(10):
-#    t = threading.Thread(target=scrapeWorker, args = (q,str(i),"mobotix",mdb['cams'],gip))
-#    t.start()
+q = Queue.Queue()
+dork = "panamjpg"
+scrapequeue(dork,q)
+for i in range(numthreads):
+    t = threading.Thread(target=scrapeWorker, args = (q,str(i),dork,mdb['cams'],gip))
+    t.start()
 
-stuff = getnpages(search_panamjpg,3)
-for item in stuff:
-    print "foo"
-    print checksite(item,"panamjpg")
-    #capturesite(item,"panamjpg")
+#stuff = getnpages(search_panamjpg,3)
+#for item in stuff:
+#    print "foo"
+#    print checksite(item,"panamjpg")
+#    #capturesite(item,"panamjpg")
